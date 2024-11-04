bf1933e3e57b42c65643b4e1c4cddc637438107f
Niwesh Sah <sahniwesh@gmail.com>
2024-11-04 22:24:06 +0530

    addNewAttendee added

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true }); // Important to merge params from the parent route
 const User = require("../models/user");
+const Attendee = require("../models/attendee");
 const { jwtAuthMiddleware, generateToken } = require("../utils/jwt");
 // const Conference = require('../models/conference');
 
@@ -81,13 +82,55 @@ router.get("/profile", jwtAuthMiddleware, async (req, res) => {
 });
 
 router.get("/userTesting", async (req, res) => {
-    try {
-        // const users = await User.find();
-        res.status(200).json( {error: "testing"});
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+  try {
+    // const users = await User.find();
+    res.status(200).json({ error: "testing" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/addNewAttendee", async (req, res) => {
+  try {
+    // const data = req.body; // Assuming the request body contains the person data
+
+    const data = {
+      name: req.body.name,
+      designation: req.body.designation,
+      mobile: req.body.mobile,
+      email: req.body.email,
+      about: req.body.about,
+      conferenceAttendance: req.body.conferenceAttendance,
+      eventsAttended: req.body.eventsAttended,
+      foodCouponUsed: req.body.foodCouponUsed,
+      secretQRSTring: req.body.secretQRSTring,
+      photo: req.body.photo,
+    };
+
+    // Check if the email is already registered
+    const existingAttendee = await Attendee.findOne({ email: data.email });
+    if (existingAttendee) {
+      return res.status(400).json({ error: "Email is already registered" });
     }
-    });
+    
+    const existingAttendee2 = await Attendee.findOne({ mobile: data.mobile });
+    if (existingAttendee) {
+      return res.status(400).json({ error: "Mobile is already registered" });
+    }
+
+    // Create a new Person document using the Mongoose model
+    const newAttendee = new Attendee(data);
+
+    // Save the new person to the database
+    const response = await newAttendee.save();
+    console.log("data saved");
+
+    res.status(200).json({ response: response });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Unable to add new attendee" });
+  }
+});
 
 const conferenceRoutes = require("./conferenceRoute"); // Import the conference route
 router.use("/conference", conferenceRoutes); // use the conferenceRoutes
